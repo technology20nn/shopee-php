644e898ae1c3211b308dc6518a1e46936f7f89a4
Linh Phạm <linhpv@gemvietnam.com>
2022-09-20 14:21:52 +0700

    get logistic

--- src/Nodes/Logistics/Logistics.php
+++ src/Nodes/Logistics/Logistics.php
@@ -12,6 +12,17 @@ class Logistics extends NodeAbstractV2
 {
 
     /**
+     * Use this call to get all supported Logistic Channel.
+     *
+     * @param array|RequestParametersInterface $parameters
+     * @return ResponseData
+     */
+    public function getLogistics($parameters = []): ResponseData
+    {
+        return $this->get("/api/v2/logistics/get_channel_list", ClientV2::API_TYPE_SHOP, $parameters);
+
+    }
+    /**
      * Get return list.
      *
      * @param array|RequestParametersInterface $parameters
