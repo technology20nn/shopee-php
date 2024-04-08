0a97dfd003b9549e8e7df92320a5b16ce1dbcd30
qhuy.duong <qhuy.duong@gmail.com>
2024-04-09 02:15:08 +0700

    changed: add request GetMessages

---
+++ src/Nodes/Chat/Parameters/GetMessages.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Shopee\Nodes\Chat\Parameters;
+
+use Shopee\RequestParameters;
+
+class GetMessages extends RequestParameters
+{
+    public function getMessageIdList(): int
+    {
+        return $this->parameters['message_id_list'];
+    }
+
+    /**
+     * @param int $message_id_list
+     * @return $this
+     */
+    public function setMessageIdList(int $message_id_list)
+    {
+        $this->parameters['message_id_list'] = $message_id_list;
+
+        return $this;
+    }
+
+    public function getConversationId(): int
+    {
+        return $this->parameters['conversation_id'];
+    }
+
+    /**
+     * @param int $conversation_id
+     * @return $this
+     */
+    public function setConversationId(int $conversation_id)
+    {
+        $this->parameters['conversation_id'] = $conversation_id;
+
+        return $this;
+    }
+
+    public function getPageSize(){
+        return $this->parameters['page_size'];
+    }
+
+    /**
+     * @param MessageContent
+     * @return $this
+     */
+    public function setPageSize($page_size)
+    {
+        $this->parameters['page_size'] = $page_size;
+
+        return $this;
+    }
+
+    public function getOffset(){
+        return $this->parameters['offset'];
+    }
+
+    /**
+     * @param MessageContent
+     * @return $this
+     */
+    public function setOffset($offset)
+    {
+        $this->parameters['offset'] = $offset;
+
+        return $this;
+    }
+}
